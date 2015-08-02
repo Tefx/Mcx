2f5ba5cea652ebcf3f7e8366f0aee8efdf9daae8
Zhaomeng Zhu (Tefx) <zhaomeng.zhu@gmail.com>
2015-08-02 13:54:32 +0800

    can close spited res pane now

--- do_conn.py
+++ do_conn.py
@@ -2,4 +2,6 @@ import sys
 import subprocess
 
 name = raw_input().strip()
+subprocess.check_output(["tmux", "send-keys", "Enter"])
 subprocess.check_output(["tmux", "new-window", "-n", name, ("echo 'conncetiong to %s and do sth.';read" % name)])
+#subprocess.check_output(["tmux", "split-window", "-h", ("echo 'conncetiong to %s and do sth.';read" % name)])
